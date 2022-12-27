7a57d6c3b89d3f123607e925de1fea41f99d98d2
Bria222 <techconsultants.bree@gmail.com>
2022-12-27 11:10:46 +0300

    fix response errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,7 @@ class UsersController < ApplicationController
   
     def user_params
       params.permit(
-        :avatar, :name, :username, :email, :password, :password_confirmation
+     :name, :username, :email, :password, :password_confirmation
       )
     end
   end
